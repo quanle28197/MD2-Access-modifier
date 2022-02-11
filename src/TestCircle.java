34d50eeca0fc96802f413804147f002852af7c89
quanle28197 <86665723+quanle28197@users.noreply.github.com>
2022-02-11 11:16:23 +0700

    update

---
+++ src/TestCircle.java
@@ -0,0 +1,7 @@
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle cc1 = new Circle(5);
+        System.out.println("Circle has radius: " +cc1.getRadius());
+    }
+}
+
